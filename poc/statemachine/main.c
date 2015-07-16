2f6f10c1f8ebee791df5c0aa78a24a8dbeb3c07f
Garrett Greenwood <garrettagreenwood@gmail.com>
2015-07-16 07:08:59 +0000

    WEEEEEEEEEEEEEE
    Signed-off-by: Garrett Greenwood <garrettagreenwood@gmail.com>

--- poc/statemachine/main.c
+++ poc/statemachine/main.c
@@ -2,13 +2,6 @@
 #include <stdint.h>
 
 
-/*
-typedef struct State_t {
-	struct State_t (*next_fn)(void);
-	uint8_t counter;
-} State;
-*/
-
 typedef uint8_t (*Color)(uint8_t counter);
 
 typedef struct State_t {
@@ -18,42 +11,32 @@ typedef struct State_t {
 	Color blue;
 } State;
 
-// Simple color animation
-uint8_t increment(uint8_t counter) {
+// Simple color ramp
+uint8_t linear(uint8_t counter) {
 	return counter;
 }
 
-State curState = {0, increment, increment, increment};
-
-/*
-State animation(void);
-State idle(void);
-
-State animation(void)
-{
-	printf("Entering animation, frame \n");
-	State st = {animation, 0};
-	return st;
+uint8_t zero(uint8_t counter) {
+	return 0;
 }
 
-State idle(void)
-{
-	printf("Entering idle\n");
-	State st = {idle, 0};
-	return st;
+// Start on a simple purple and ramp up with counter
+State curState = {0, linear, zero, linear};
+
+void nextFrame(State *st) {
+	st->counter++;
 }
-*/
+
 
 int main(int arc, char **argv)
 {
 	printf("Entering main\n");
 
-	//State curState = {animation};
-	
-	for(curState.counter = 0; curState.counter < 10; curState.counter++) {
+	int i;
+	for(i = 0; i < 10; i++) {
+		nextFrame(&curState);
 		printf("counter: %u\tred: %u\t green: %u\tblue: %u\n", curState.counter, curState.red(curState.counter), curState.blue(curState.counter), curState.green(curState.counter));
 	}
-
 
 	return 0;
 }
