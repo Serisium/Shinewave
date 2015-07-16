00f18a9c812c96bd456d692b033a15387dd1d7af
Garrett Greenwood <garrettagreenwood@gmail.com>
2015-07-16 07:37:31 +0000

    made the code neater and left notes for the morning
    Signed-off-by: Garrett Greenwood <garrettagreenwood@gmail.com>

--- poc/statemachine/main.c
+++ poc/statemachine/main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdint.h>
 
+typedef uint8_t (*Color_fn)(uint8_t counter);
 
-typedef uint8_t (*Color)(uint8_t counter);
+typedef struct Color_t {
+	uint8_t red;
+	uint8_t green;
+	uint8_t blue;
+} Color;
 
+// TODO: Find out a way to do exit conditions
 typedef struct State_t {
 	uint8_t counter;
-	Color red;
-	Color green;
-	Color blue;
+	Color_fn red;
+	Color_fn green;
+	Color_fn blue;
 } State;
 
 // Simple color ramp
@@ -23,10 +29,18 @@ uint8_t zero(uint8_t counter) {
 // Start on a simple purple and ramp up with counter
 State curState = {0, linear, zero, linear};
 
+// TODO: Add exit condition processing
 void nextFrame(State *st) {
 	st->counter++;
 }
 
+Color getColor(State *st) {
+	uint8_t red = st->red(st->counter);
+	uint8_t green = st->green(st->counter);
+	uint8_t blue = st->blue(st->counter);
+	Color col = {red, green, blue};
+	return col;
+}
 
 int main(int arc, char **argv)
 {
@@ -34,8 +48,9 @@ int main(int arc, char **argv)
 
 	int i;
 	for(i = 0; i < 10; i++) {
+		Color col = getColor(&curState);
+		printf("counter: %u\tred: %u\t green: %u\tblue: %u\n", curState.counter, col.red, col.green, col.blue);
 		nextFrame(&curState);
-		printf("counter: %u\tred: %u\t green: %u\tblue: %u\n", curState.counter, curState.red(curState.counter), curState.blue(curState.counter), curState.green(curState.counter));
 	}
 
 	return 0;
