dcd5c9b48eb112f7c52c7005a21766596b1b26aa
Garrett Greenwood <garrettagreenwood@gmail.com>
2015-07-17 06:50:47 +0000

    Added exit condition on max frame
    Signed-off-by: Garrett Greenwood <garrettagreenwood@gmail.com>

--- poc/statemachine/main.c
+++ poc/statemachine/main.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdint.h>
 
-typedef uint8_t (*Color_fn)(uint8_t counter);
+typedef struct Color Color;
+typedef struct State State;
 
-typedef struct Color_t {
-	uint8_t red;
-	uint8_t green;
-	uint8_t blue;
-} Color;
+typedef uint8_t (*Color_fn)(uint8_t counter);
+typedef uint8_t (*Condition)(uint8_t counter, uint8_t parameter);
+
+struct Color {
+	uint8_t red, green, blue;
+};
 
 // TODO: Find out a way to do exit conditions
-typedef struct State_t {
+struct State {
 	uint8_t counter;
-	Color_fn red;
-	Color_fn green;
-	Color_fn blue;
-} State;
+	Color_fn red, green, blue;
+	uint8_t max;
+	State *nextState;
+};
 
 // Simple color ramp
 uint8_t linear(uint8_t counter) {
@@ -26,12 +28,14 @@ uint8_t zero(uint8_t counter) {
 	return 0;
 }
 
-// Start on a simple purple and ramp up with counter
-State curState = {0, linear, zero, linear};
-
 // TODO: Add exit condition processing
 void nextFrame(State *st) {
-	st->counter++;
+	if(st->counter >= st->max) {
+		st->counter = 0;
+		st = st->nextState;
+	} else {
+		st->counter++;
+	}
 }
 
 Color getColor(State *st) {
@@ -42,15 +46,19 @@ Color getColor(State *st) {
 	return col;
 }
 
+State idle = {0, linear, zero, zero, 2, &idle};
+
 int main(int arc, char **argv)
 {
 	printf("Entering main\n");
 
+	State *curState = &idle;
+
 	int i;
 	for(i = 0; i < 10; i++) {
-		Color col = getColor(&curState);
-		printf("counter: %u\tred: %u\t green: %u\tblue: %u\n", curState.counter, col.red, col.green, col.blue);
-		nextFrame(&curState);
+		Color col = getColor(curState);
+		printf("counter: %u\tred: %u\t green: %u\tblue: %u\n", curState->counter, col.red, col.green, col.blue);
+		nextFrame(curState);
 	}
 
 	return 0;
