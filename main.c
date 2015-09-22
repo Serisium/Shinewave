bd92da3baf7fc6d432b21424bc7fd743e93aae93
Garrett Greenwood <garrettagreenwood@gmail.com>
2015-09-22 05:51:46 -0500

    Gamecube controller is now a joystick (still labeled as mouse)
    Signed-off-by: Garrett Greenwood <garrettagreenwood@gmail.com>

--- main.c
+++ main.c
@@ -141,6 +141,50 @@ void setup_usb(void) {
     sei();
 }
 
+void build_report(Controller *controller, report_t report) {
+    if(CONTROLLER_A(*controller))
+        reportBuffer.buttonMask |= (1 << 0);
+    else
+        reportBuffer.buttonMask &= ~(1 << 0);
+
+    if(CONTROLLER_B(*controller))
+        reportBuffer.buttonMask |= (1 << 1);
+    else
+        reportBuffer.buttonMask &= ~(1 << 1);
+
+    if(CONTROLLER_X(*controller))
+        reportBuffer.buttonMask |= (1 << 2);
+    else
+        reportBuffer.buttonMask &= ~(1 << 2);
+
+    if(CONTROLLER_Y(*controller))
+        reportBuffer.buttonMask |= (1 << 3);
+    else
+        reportBuffer.buttonMask &= ~(1 << 3);
+
+    if(CONTROLLER_START(*controller))
+        reportBuffer.buttonMask |= (1 << 7);
+    else
+        reportBuffer.buttonMask &= ~(1 << 7);
+
+    if(CONTROLLER_D_UP(*controller))
+        reportBuffer.buttonMask |= (1 << 6);
+    else
+        reportBuffer.buttonMask &= ~(1 << 6);
+
+    if(controller->analog_l > 127)
+        reportBuffer.buttonMask |= (1 << 4);
+    else
+        reportBuffer.buttonMask &= ~(1 << 4);
+
+    reportBuffer.x = controller->joy_x;
+    reportBuffer.y = -(controller->joy_y);
+    reportBuffer.z = controller->analog_l;
+    reportBuffer.rx = controller->c_x;
+    reportBuffer.ry = controller->c_y;
+    reportBuffer.rz = controller->analog_r;
+}
+
 int main(void)
 {
     setup_pins();
@@ -170,14 +214,8 @@ int main(void)
             // Try to grab the controller state
             while(!request_message(message_buffer)) {}
 
-            if(CONTROLLER_A(*controller))
-                reportBuffer.buttonMask |= 0x01;
-            else
-                reportBuffer.buttonMask &= ~0x01;
+            build_report(controller, reportBuffer);
 
-            //reportBuffer.dx = 10;
-            reportBuffer.dx = (int8_t) controller->joy_x - 127;
-            reportBuffer.dy = (int8_t) -(controller->joy_y - 127);
             usbSetInterrupt((void *)&reportBuffer, sizeof(reportBuffer));
         }
     }
--- usb.c
+++ usb.c
@@ -1,34 +1,32 @@
 #include "usbdrv/usbdrv.h"
 
-PROGMEM const char usbHidReportDescriptor[52] = { /* USB report descriptor, size must match usbconfig.h */
+PROGMEM const char usbHidReportDescriptor[48] = {
     0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    0x09, 0x02,                    // USAGE (Mouse)
+    0x09, 0x05,                    // USAGE (Game Pad)
     0xa1, 0x01,                    // COLLECTION (Application)
-    0x09, 0x01,                    //   USAGE (Pointer)
-    0xA1, 0x00,                    //   COLLECTION (Physical)
-    0x05, 0x09,                    //     USAGE_PAGE (Button)
-    0x19, 0x01,                    //     USAGE_MINIMUM
-    0x29, 0x03,                    //     USAGE_MAXIMUM
-    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
-    0x95, 0x03,                    //     REPORT_COUNT (3)
-    0x75, 0x01,                    //     REPORT_SIZE (1)
-    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
-    0x95, 0x01,                    //     REPORT_COUNT (1)
-    0x75, 0x05,                    //     REPORT_SIZE (5)
-    0x81, 0x03,                    //     INPUT (Const,Var,Abs)
-    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
-    0x09, 0x30,                    //     USAGE (X)
-    0x09, 0x31,                    //     USAGE (Y)
-    0x09, 0x38,                    //     USAGE (Wheel)
-    0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
-    0x25, 0x7F,                    //     LOGICAL_MAXIMUM (127)
-    0x75, 0x08,                    //     REPORT_SIZE (8)
-    0x95, 0x03,                    //     REPORT_COUNT (3)
-    0x81, 0x06,                    //     INPUT (Data,Var,Rel)
-    0xC0,                          //   END_COLLECTION
-    0xC0,                          // END COLLECTION
+    0x05, 0x09,                    //   USAGE_PAGE (Button)
+    0x19, 0x01,                    //   USAGE_MINIMUM (Button 1)
+    0x29, 0x10,                    //   USAGE_MAXIMUM (Button 16)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+    0x75, 0x01,                    //   REPORT_SIZE (1)
+    0x95, 0x10,                    //   REPORT_COUNT (16)
+    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+    0x05, 0x01,                    //   USAGE_PAGE (Generic Desktop)
+    0x09, 0x30,                    //   USAGE (X)
+    0x09, 0x31,                    //   USAGE (Y)
+    0x09, 0x32,                    //   USAGE (Z)
+    0x09, 0x33,                    //   USAGE (Rx)
+    0x09, 0x34,                    //   USAGE (Ry)
+    0x09, 0x35,                    //   USAGE (Rz)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x26, 0xff, 0x00,              //   LOGICAL_MAXIMUM (255)
+    0x75, 0x08,                    //   REPORT_SIZE (8)
+    0x95, 0x06,                    //   REPORT_COUNT (6)
+    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+    0xc0                           //   END_COLLECTION
 };
+
 
 /* This is the same report descriptor as seen in a Logitech mouse. The data
  * described by this descriptor consists of 4 bytes:
@@ -37,11 +35,17 @@ PROGMEM const char usbHidReportDescriptor[52] = { /* USB report descriptor, size
  *     Y7 Y6 Y5 Y4 Y3 Y2 Y1 Y0 .... 8 bit signed relative coordinate y
  *     W7 W6 W5 W4 W3 W2 W1 W0 .... 8 bit signed relative coordinate wheel
  */
+/*
 typedef struct{
     uchar   buttonMask;
     char    dx;
     char    dy;
     char    dWheel;
+} report_t;
+*/
+typedef struct{
+    uint16_t buttonMask;
+    uint8_t x, y, z, rx, ry, rz;
 } report_t;
 
 static report_t reportBuffer;
